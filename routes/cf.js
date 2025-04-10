7fb8b1efe81c018fecc2e0f93dc4565d30c136fc
jjc <jjc@666>
2025-04-10 16:45:34 +0800

    add

--- routes/cf.js
+++ routes/cf.js
@@ -6,11 +6,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const router = express.Router();
 
-router.get("/cf", (req, res) => {
-    const list =''
-    const txtPath= join(__dirname, "cf.txt");
+router.get("/main", (req, res) => {
+    const txtPath= join(__dirname, "main.txt");
     const txt = fs.readFileSync(txtPath, "utf-8");
     res.send(txt);
 });
+
+router.get("/cf", (req, res) => {
+    const txtPath= join(__dirname, "cf.txt");
+    const txt = fs.readFileSync(txtPath, "utf-8");
+    res.send(txt);
+})
 
 export default router;
